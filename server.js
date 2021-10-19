c95bb2815abf2ccb82ffdf2161727daa0f9a54be
kboston91 <kourtneygboston@gmail.com>
2021-10-19 18:20:55 -0500

    added logic to display the information from the candidate table

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,11 +8,30 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Kgb9893682886',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
+//returns the data from the candidates table
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
+//Catchall route
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
